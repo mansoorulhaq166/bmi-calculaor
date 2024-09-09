3f44790867b3dc5927f8fc34f4f91c8683edd749
Mansoor ul haq <mansoorulhaq166@gmail.com>
2024-09-09 05:06:27 +0500

    Project Updated

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.bmicalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-<<<<<<< HEAD
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -35,10 +34,31 @@ class MainActivity : AppCompatActivity() {
             val heightFeet = binding.editHeightFeet.text.toString().toDoubleOrNull()
             val heightInches = binding.editHeightInches.text.toString().toDoubleOrNull()
 
-            if (weight != null && heightFeet != null && heightInches != null) {
-                viewModel.calculateBMI(weight, heightFeet, heightInches)
+            var isValid = true
+
+            if (weight == null) {
+                binding.editWeightLayout.error = "Please enter a valid weight"
+                isValid = false
             } else {
-                Toast.makeText(this, "Please Fill Required Fields", Toast.LENGTH_SHORT).show()
+                binding.editWeightLayout.error = null
+            }
+
+            if (heightFeet == null) {
+                binding.editHeightFeetLayout.error = "Please enter a valid height (feet)"
+                isValid = false
+            } else {
+                binding.editHeightFeetLayout.error = null
+            }
+
+            if (heightInches == null) {
+                binding.editHeightInchesLayout.error = "Please enter a valid height (inches)"
+                isValid = false
+            } else {
+                binding.editHeightInchesLayout.error = null
+            }
+
+            if (isValid) {
+                viewModel.calculateBMI(weight!!, heightFeet!!, heightInches!!)
             }
         }
 
@@ -47,70 +67,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         viewModel.bmiCategory.observe(this) { category ->
+            binding.textMsg.visibility = View.VISIBLE
             binding.textMsg.text = category
         }
 
         viewModel.backgroundColor.observe(this) { colorResId ->
             binding.mainLnrLayout.setBackgroundColor(ContextCompat.getColor(this, colorResId))
         }
-=======
-import kotlinx.android.synthetic.main.activity_main.*
-
-@Suppress("DEPRECATION")
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        button_calculate.setOnClickListener {
-            if (edit_weight.text.toString() != "" && edit_height_feet.text.toString() != ""
-                && edit_height_inches.text.toString() != ""
-            ) {
-                val wt = (edit_weight.text.toString()).toDouble()
-                val htFeet = (edit_height_feet.text.toString()).toDouble()
-                val htInch = (edit_height_inches.text.toString()).toDouble()
-
-                val totalInches = (htFeet * 12) + htInch
-                val totalCentimeter = totalInches * 2.54
-
-                val totalMeter = totalCentimeter / 100
-
-                val bmi = wt / (totalMeter * totalMeter)
-
-                text_title.text = "Your BMI"
-                text_result.text = "${bmi.toInt()}"
-
-                when {
-                    bmi > 25 -> {
-                        Toast.makeText(this, "You are OverWeight", Toast.LENGTH_SHORT).show()
-
-                        text_msg.text = "You are OverWeight"
-
-                        main_lnr_layout.setBackgroundColor(resources.getColor(R.color.colorRed))
-
-                    }
-                    bmi < 18 -> {
-                        Toast.makeText(this, "You are UnderWeight", Toast.LENGTH_SHORT).show()
-
-                        text_msg.text = "You are UnderWeight"
-
-                        main_lnr_layout.setBackgroundColor(resources.getColor(R.color.colorBlue))
-
-                    }
-                    else -> {
-                        Toast.makeText(this, "You are Healthy", Toast.LENGTH_SHORT).show()
-
-                        text_msg.text = "You are Healthy"
-
-                        main_lnr_layout.setBackgroundColor(resources.getColor(R.color.colorGreen))
-                    }
-                }
-
-            } else {
-                Toast.makeText(this@MainActivity, "Please Fill Required Fields", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }
->>>>>>> 87453b12eea9c094d1bff880b000a0ec26c60fcd
     }
 }
